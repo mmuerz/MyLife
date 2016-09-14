2e8fd9849740e06e1b916a9c0a098d19080ff402
mmuerz <marius.muerz@gmx.de>
2016-09-14 15:34:07 +0200

    Create RootLayout and login Scene package, start filling HomeScene

--- src/main/java/controll/home/MainApplet.java
+++ src/main/java/controll/home/MainApplet.java
@@ -2,4 +2,16 @@ package controll.home;
 
 public class MainApplet {
 
+	private static MainApplet instance = null;
+
+	public static MainApplet getInstance() {
+		if (instance == null)
+			instance = new MainApplet();
+		return instance;
+	}
+
+	private MainApplet() {
+
+	}
+
 }
---
+++ src/main/java/view/root/RootLayoutController.java
@@ -0,0 +1,42 @@
+package view.root;
+
+import controll.home.MainApplet;
+import javafx.fxml.FXML;
+
+/**
+ * The controller for the root layout. The root layout provides the basic
+ * application layout containing a menu bar and space where other JavaFX
+ * elements can be placed.
+ * 
+ * @author MMuerz
+ */
+public class RootLayoutController {
+
+	// Reference to the main application
+	private static RootLayoutController instance;
+	private MainApplet mainApp = MainApplet.getInstance();
+
+	/**
+	 * Is called by the main application to give a reference back to itself.
+	 * 
+	 * @param mainApp
+	 */
+	public void setMainApplet(MainApplet mainApp) {
+		this.mainApp = mainApp;
+	}
+
+	/**
+	 * Creates an empty address book.
+	 */
+	@FXML
+	private void handleNew() {
+		if (mainApp != null) {
+		}
+	}
+
+	public static RootLayoutController getInstance() {
+		if (instance == null)
+			instance = new RootLayoutController();
+		return instance;
+	}
+}
